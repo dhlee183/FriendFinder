6ebaa3d9d210c5c7c63697ba3bcc179226a3c5bb
David Hyun Lee <hyun183@Davids-MacBook-Pro.local>
2016-11-03 00:11:44 -0400

    Updated all the files

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -5,7 +5,29 @@ module.exports = function (app) {
 		res.json(friendData);
 	});
 
-	app.post('/api/friends', function (req, res) {
-		
-	})
-}
+	app.post('/api/friends', function(req,res){
+		var newFriendData = req.body.scores;
+		var scoresArray = [];
+		var bestMatch = 0;
+
+		for (var i = 0; i < friendData.length; i++){
+			var scoresDiff = 0;
+
+			for (var j = 0; j < newFriendData.length; j++){
+				scoresDiff += (Math.abs(parseInt(friendData[i].scores[j]) - parseInt(newFriendData[j])));
+			}
+
+			scoresArray.push(scoresDiff);
+		}
+
+		for (var i = 0; i < scoresArray.length; i++){
+			if(scoresArray[i] <= scoresArray[bestMatch]){
+			bestMatch = i;
+			}
+		}
+
+		res.json(friendData[bestMatch]);
+
+		friendData.push(req.body);
+	});
+};
