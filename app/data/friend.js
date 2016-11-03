f3002336a89d49d574780f32e10a60520bfa938b
David Hyun Lee <hyun183@Davids-MacBook-Pro.local>
2016-11-02 23:01:15 -0400

    Updated the js and html files

---
+++ app/data/friend.js
@@ -0,0 +1,20 @@
+var friendArray = [
+	{
+		name: 'David',
+		photo: 'https://www.facebook.com/photo.php?fbid=1500746806906359&l=57cea56df8',
+		scores: [
+			'4',
+			'3',
+			'5',
+			'1',
+			'2',
+			'3',
+			'4',
+			'5',
+			'3',
+			'2'
+		]
+	}
+];
+
+module.exports = friendArray;
